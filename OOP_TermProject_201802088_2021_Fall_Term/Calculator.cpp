e594d8d6705ebec6b865ffb28d21b54c051e1b91
201802088@o.cnu.ac.kr <201802088@o.cnu.ac.kr>
2021-10-30 15:44:27 +0900

    초기 커밋.

---
+++ OOP_TermProject_201802088_2021_Fall_Term/Calculator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stack>
+#include<string>
+
+
+
+int main() {
+
+	std::string numericalExpression;
+	getline(std::cin, numericalExpression);
+	std::cout << numericalExpression << std::endl;
+
+	return 0;
+}
