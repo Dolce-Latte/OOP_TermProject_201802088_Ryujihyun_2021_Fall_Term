4cebb28c5531041add7f582ed58a2adbf8525df9
201802088@o.cnu.ac.kr <201802088@o.cnu.ac.kr>
2021-11-08 15:50:25 +0900

    add command 진행중

--- OOP_TermProject_201802088_2021_Fall_Term/Calculator.cpp
+++ OOP_TermProject_201802088_2021_Fall_Term/Calculator.cpp
@@ -4,17 +4,86 @@
 
 using namespace std;
 
+class Command {
+public:
+	virtual int calculationExecute(int firstOperand, int secondOperand) = 0;
+};
+
+class Addition_Command : public Command {
+public:
+	Addition_Command()
+};
+
+class Calculator {
+private:
+	Expressions* expressions;
+	stack<int> intStack;
+	int result = 0;
+public:
+	
+};
+
+class Calculations {
+private:
+	int result;
+	string expressions;
+public:
+	Calculations(string expressions, int result) {
+		setExpressions(expressions);
+		setResult(result);
+	}
+
+	void setResult(int result) {
+		this->result = result;
+	}
+	void setExpressions(string expressions) {
+		this->expressions = expressions;
+	}
+
+	int getResult() {
+		return result;
+	}
+	string getExpressions() {
+		return expressions;
+	}
+
+	int calculate();
+};
+
+int Calculations::calculate() {
+	string str = this->getExpressions();
+	int answer = this->getResult();
+	stack<int> intStack;
+
+	for (int i = 0; str[i] != NULL; i++) {
+		char token = str[i];
+		answer = 0;
+
+		if (token == ' ') { continue; }
+		if ('0' <= token && token <= '9') {
+			answer = answer * 10 + (token - '0');
+			token = str[++i];
+		}
+		else {
+			int firstOperand = intStack.top();
+			intStack.pop();
+			int secondOperand = intStack.top();
+			intStack.pop();
+
+			switch (token) {
+
+			}
+		}
+	}
+}
 class Expressions {
 private:
 	string postExpressions;
 	string inExpressions;
-	int answer = 0;
 public:
 	Expressions(string inExpressions) {
-		this->inExpressions = inExpressions;
+		setInExpressions(inExpressions);
 	}
-
-	~Expressions() {};
 
 	void setInExpressions(string inExpressions) {
 		this->inExpressions = inExpressions;
@@ -29,13 +98,43 @@ public:
 	string getPostExpressions() {
 		return postExpressions;
 	}
-
-	void inFixToPostFix(string inExpressions);
+	
+	void changeNumberInExpr();
+	void inFixToPostFix();
+	void hexaDecimal(int& index, string& before, string& after);
+	void binary(int& index, string& before, string& after);
 	bool getPriority(char stackOperand, char currentOperand);
 };
 
-void Expressions::inFixToPostFix(string inExpressions)
+void Expressions::changeNumberInExpr()
 {
+	string str = this->getInExpressions();
+	int len = str.length();
+	string nStr;
+
+	for (int i = 0; (i < len) && (str[i] != NULL); i++) {
+		char token = str[i];
+		if ('0' <= token && token <= '9') {
+			if (token == '0') {
+				if (str[i + 1] == 'x') {
+					hexaDecimal(i, str, nStr);
+				}
+				else if (str[i + 1] == 'b') {
+					binary(i, str, nStr);
+				}
+				else { nStr += token; }
+			}
+			else { nStr += token; }
+		}
+		else { nStr += token; }
+	}
+	setInExpressions(nStr);
+	return;
+}
+
+void Expressions::inFixToPostFix()
+{	
+	string str = this->getInExpressions();
 	stack<char> charStack;
 	string postFix;
 
@@ -118,6 +217,36 @@ void Expressions::inFixToPostFix(string inExpressions)
 	return;
 }
 
+void Expressions::hexaDecimal(int& index, string& before, string& after)
+{
+	int len = before.length();
+	int j;
+	int hex = 0;
+	string tmp;
+	for (j = index + 2; (j < len) && (('0' <= before[j] && before[j] <= '9') || ('a' <= before[j] && before[j] <= 'f')); j++) {
+		tmp += before[j];
+	}
+	hex = stoi(tmp, nullptr, 16);
+	after += to_string(hex);
+	index = j - 1;
+	return;
+}
+
+void Expressions::binary(int& index, string& before, string& after)
+{
+	int len = before.length();
+	int j;
+	int bin = 0;
+	string tmp;
+	for (j = index + 2; (j < len) && ('0' <= before[j] && before[j] <= '1'); j++) {
+		tmp += before[j];
+	}
+	bin = stoi(tmp, nullptr, 2);
+	after += to_string(bin);
+	index = j - 1;
+	return;
+}
+
 bool Expressions::getPriority(char stackOperand, char currentOperand)
 {
 	if (stackOperand == '(') { return false; }
@@ -130,7 +259,13 @@ bool Expressions::getPriority(char stackOperand, char currentOperand)
 }
 
 int main() {
-	Expressions* expressions;
+	string exp;
+	string re;
+	cin >> exp;
+	Expressions* expressions = new Expressions(exp);
+	expressions->changeNumberInExpr();
+	expressions->inFixToPostFix();
+	cout << expressions->getPostExpressions() << endl;
 
-
+	return 0;
 }
