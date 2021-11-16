6d582136115432b9cb0fedf3728c27f8059c5f5e
201802088@o.cnu.ac.kr <201802088@o.cnu.ac.kr>
2021-11-16 15:14:54 +0900

    Expressions 파일 분리중

---
+++ OOP_TermProject_201802088_2021_Fall_Term/Expressions.hpp
@@ -0,0 +1,14 @@
+#ifndef _EXPRESSIONS_H_
+#define _EXPRESSIONS_H_
+#include<iostream>
+#include<string>
+
+class Expressions {
+private:
+	std::string _expr;
+public:
+
+	Expressions() : _expr(expr){}
+};
+
+#endif
