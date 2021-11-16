ef314a10cb56195688e522e7a837855cc7e2b43c
201802088@o.cnu.ac.kr <201802088@o.cnu.ac.kr>
2021-11-17 00:51:28 +0900

    종료과정 수정

--- OOP_TermProject_201802088_2021_Fall_Term/main.cpp
+++ OOP_TermProject_201802088_2021_Fall_Term/main.cpp
@@ -6,10 +6,13 @@ int main() {
 	Facade* calculator;
 
 	while (true) {
-		std::cout << "수식을 입력하세요.(종료 : QUIT)" << std::endl;
+		std::cout << "수식을 입력하세요.(종료 : QUIT or EOF)" << std::endl;
 		std::cin >> _input;
 
-		if (_input == "QUIT") { break; }
+		if (_input == "QUIT" || std::cin.eof()) { 
+			std::cout << "계산기를 종료합니다." << std::endl << std::endl;
+			break;
+		}
 		calculator = new Facade(_input);
 
 		try{
@@ -17,10 +20,10 @@ int main() {
 			calculator->operationCalc();
 		}
 		catch (std::logic_error& e) {
-			std::cout << e.what() << std::endl;
+			std::cout << e.what() << std::endl << std::endl;
 		}
 		catch (...) {
-			std::cout << "알 수 없는 오류" << std::endl;
+			std::cout << "알 수 없는 오류" << std::endl << std::endl;
 		}
 	}
 
